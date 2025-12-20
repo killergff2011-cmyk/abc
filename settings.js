5bd5fff3e17291b037f531df015064922ba33021
killergff2011-cmyk <killergff2011@gmail.com>
2025-12-20 12:39:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'A𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=f0d7df8066ff0f6e' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'A𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=f0d7df8066ff0f6' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
